d98e9eca3e1db7e94c8f4943c8de60e8a7da53b7
Leul Regassa <leulregasa333@gmail.com>
2026-06-13 17:52:35 +0300

    Day4- Loops

--- Day4-Loops/main.py
+++ Day4-Loops/main.py
@@ -1,6 +1,27 @@
 if __name__ == '__main__':
-    
+
     n=int(input("type any number: "))
 
     for i in range(n):
         print(i * i)
+
+    # sum of n numbers:
+
+    n=int(input("type any number: "))
+
+    # 0 upto n-1
+    total=0
+
+    for i in range(n):
+        total=total + i
+        print(total)
+
+      #1 upto n-1
+    n=int(input("type any number: "))
+
+    total=0
+
+    for i in range(1,n+1):
+
+        total=total + i
+        print(total)
